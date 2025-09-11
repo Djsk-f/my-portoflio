f1a77bd043d8641123cb86ac19201733980ecc75
Fidele Loffou <fidele.kounga@londo-tech.com>
2025-09-11 14:55:08 +0100

    modified projects descriptions

--- src/app/mocks/projects.mock.ts
+++ src/app/mocks/projects.mock.ts
@@ -7,12 +7,7 @@ export const projects: Project[] = [
     title: "ClICK CADYST",
     role: "Full-Stack Developer",
     description:
-      "Développement d'une application web et mobile pour la gestion des commandes internes de sacs de farine chez PASTA. UI réalisée avec Ionic & Angular, API sécurisée en Node.js, base de données MongoDB.",
-    link: {
-      url: "https://play.google.com/store/apps/details?id=com.pasta.clickcadyst",
-      label: "Version mobile sur Google Play",
-      icon: FaGooglePlay,
-    },
+      "Développement d'une application web et mobile pour la gestion des commandes internes de sacs de farine chez PASTA. UI réalisée avec Ionic & Angular, API sécurisée en Nest.js, base de données MongoDB.",
     stack: ["Ionic", "Angular", "Node.js", "MongoDB"],
     image: "/assets/images/cadyst_mobile.png",
   },
@@ -20,27 +15,23 @@ export const projects: Project[] = [
     title: "BCI MOBILE",
     role: "Full-Stack Developer",
     description:
-      "API pour demandes d’augmentation de plafond de paiement hors zone CEMAC. Intégration dans le portail client avec suivi, notification et reporting. Réalisé avec Node.js, MongoDB, et Angular.",
-    link: {
-      url: "#",
-      label: "Fly Banking BICEC",
-    },
-    stack: ["Node.js", "MongoDB", "Angular"],
+      "Plateforme mobile pour la gestion des operations bancaires des client de la BCI. Réalisé avec Node.js, MongoDB, et Angular.",
+    stack: ["Nest.js", "MongoDB", "Angular"],
     image: "/assets/images/bci_mobile.png",
   },
   {
     title: "My CIMENCAM",
     role: "Full-Stack Developer",
     description:
-      "Plateforme e-commerce web pour la gestion des commandes, du catalogue produits et des paiements. Stack utilisée : Angular, Node.js, MongoDB.",
-    stack: ["Angular", "Node.js", "MongoDB"],
+      "Plateforme de d'achats des sacs de ciment pour les clients de l'entreprise CIMENCAM. Stack utilisée : Angular, Node.js, MongoDB.",
+    stack: ["Angular", "Nest.js", "MongoDB"],
     image: "/assets/images/cimencam.png",
   },
   {
     title: "BCI NET",
     role: "Full-Stack Developer",
     description:
-      "Plateforme e-commerce web pour la gestion des commandes, du catalogue produits et des paiements. Stack utilisée : Angular, Node.js, MongoDB.",
+      "Plateforme de gestion des operations bancaires de la BCI. Stack utilisée : Angular, Node.js, MongoDB.",
     stack: ["Angular", "Node.js", "MongoDB"],
     image: "/assets/images/bcinet.png",
   },
